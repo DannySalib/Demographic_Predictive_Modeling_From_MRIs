75871c6c224c1ddbeeac02f8119b0832a789a1f8
DannySalib <dhs1224@gmail.com>
2025-06-29 22:23:14 -0400

    setting up model

--- run_model_report.py
+++ run_model_report.py
@@ -1,23 +1,27 @@
 import pandas as pd 
 
-print('Getting Participants Data Frame...')
-print('__________________________________')
-df_path = r'Data\participants.tsv'
+def main():
+    print('Getting Participants Data Frame...')
+    print('__________________________________')
+    df_path = r'Data\participants.tsv'
 
-try:
-    df = pd.read_csv(df_path, sep='\t')
-except FileNotFoundError as e:
-    raise FileNotFoundError(f'{e}\nTry running `download_data.py` first!')
+    try:
+        df = pd.read_csv(df_path, sep='\t')
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f'{e}\nTry running `download_data.py` first!')
 
-print(df.head())
-print()
-
-print('Class prioirs...')
-print('________________')
-classification_group_names = ['AgeGroup', 'Child_Adult', 'Gender']
-
-for name in classification_group_names:
-    print(f'Classification Name: {name}')
-    for classification_group_name, df_classification_group in df.groupby(name):
-        print(f'P(Y={classification_group_name}) = {len(df_classification_group) / len(df) * 100:.2f}%')
+    print(df.head())
     print()
+
+    print('Class prioirs...')
+    print('________________')
+    classification_group_names = ['AgeGroup', 'Child_Adult', 'Gender']
+
+    for name in classification_group_names:
+        print(f'Classification Name: {name}')
+        for classification_group_name, df_classification_group in df.groupby(name):
+            print(f'P(Y={classification_group_name}) = {len(df_classification_group) / len(df) * 100:.2f}%')
+        print()
+
+if __name__ == '__main__':
+    main()
